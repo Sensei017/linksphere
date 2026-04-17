becce613d684bbb9df07f97a6ba31ac4c325a6a1
Sensei017 <jishnudebbiswas2.o@gmail.com>
2026-04-17 20:56:28 +0530

    fix base path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/linksphere/',  // your GitHub repo name
+  base: '/',   // ✅ FIXED for Vercel
 })
